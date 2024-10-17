52bad6dd40106bd511c8c5088921399a57fa2310
algmarques <algmarques@protonmail.com>
2024-10-17 18:06:07 +0100

    partition according to file format size

--- pydub/speech_segment.py
+++ pydub/speech_segment.py
@@ -3,6 +3,8 @@ SpeechSegment is an object implementing specific methods to deal with
 speech audio data.
 """
 
+from math import ceil
+
 from numpy import ndarray, ones, zeros, dot, int64, argmin
 from numpy import transpose, arange
 
@@ -30,56 +32,53 @@ class SpeechSegment(AudioSegment):
             segment = self._spawn(data = waveform)
             yield segment
 
-    def partition(self, max_size):
+    def partition(self, max_size, format=None):
         """
         Partitions audio segments in moments of silence according to a 
         specific heuristic.
         max_size: int (in bytes)
         """
-        
-        if self.frame_count() * self.frame_width > max_size:
 
-            max_length = max_size / self.frame_width
-            conv_width = 1 + self.frame_rate // 2
-            conv_offset = self.frame_rate // 8
-            slack = max_length // 4
-
-            # compute the amplitudes and the convolution
-            waveform = self._waveform.mean(axis=1)
-            amplitudes = waveform ** 2
-            convolved_amplitudes, mapping = convolve(
-                amplitudes,
-                conv_width,
-                conv_offset
-            )
-
-            # find the mid-points
-            internal_max_length = max_length - slack
-            n_partitions = self.frame_count() // internal_max_length
-            mid_points = arange(
-                internal_max_length,
-                (n_partitions + 1) * internal_max_length,
-                internal_max_length,
-                dtype=int64
-            )
-
-            # find a most silent point around the mid-points, this is where the 
-            # waveform will be sliced
-            cut_points = zeros(len(mid_points) + 2, dtype=int64)
-            cut_points[0] = 0
-            cut_points[-1] = self.frame_count() - 1
-            for i in range(len(mid_points)):
-                cut_points[i + 1] = get_silence(
-                    convolved_amplitudes,
-                    mapping,
-                    mid_points[i] - slack,
-                    mid_points[i] + slack
-                )
-
-            # partition the waveform
-            for i in range(len(cut_points) - 1):
-                waveform = self._waveform[cut_points[i]: cut_points[i + 1]]
-                segment = self._spawn(data = waveform)
-                yield segment
+        if format is None:
+            n_partitions = ceil(len(self.raw_data) / max_size)
         else:
-            yield self
+            size = len(self.export(format=format).read())
+            n_partitions = ceil(size / max_size)
+
+        max_length = ceil(self.frame_count() / n_partitions)
+        conv_width = 1 + self.frame_rate // 2
+        conv_offset = self.frame_rate // 8
+        slack = max_length // 4
+
+        # compute the amplitudes and the convolution
+        waveform = self._waveform.mean(axis=1)
+        amplitudes = waveform ** 2
+        convolved_amplitudes, mapping = convolve(
+            amplitudes,
+            conv_width,
+            conv_offset
+        )
+
+        # find the mid-points
+        internal_length = max_length - 2 * slack
+        n_partitions = ceil(self.frame_count() / internal_length)
+        mid_points = [i * internal_length for i in range(1, n_partitions - 1)]
+
+        # find a most silent point around the mid-points, this is where the 
+        # waveform will be sliced
+        cut_points = [None] * (n_partitions + 1)
+        cut_points[0] = 0
+        cut_points[-1] = self.frame_count() - 1
+        for i, mid_point in enumerate(mid_points):
+            cut_points[i + 1] = get_silence(
+                convolved_amplitudes,
+                mapping,
+                mid_point - slack,
+                mid_point + slack
+            )
+
+        # partition the waveform
+        for i in range(len(cut_points) - 1):
+            waveform = self._waveform[cut_points[i]: cut_points[i + 1]]
+            segment = self._spawn(data = waveform)
+            yield segment
