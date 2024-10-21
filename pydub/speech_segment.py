60cbbf727e9167717065876209ff81aab8caed91
algmarques <algmarques@protonmail.com>
2024-10-21 14:15:36 +0100

    fixed bug in SpeechSegment

--- pydub/speech_segment.py
+++ pydub/speech_segment.py
@@ -1,5 +1,5 @@
 """
-SpeechSegment is an object implementing specific methods to deal with 
+SpeechSegment is an object implementing specific methods to deal with
 speech audio data.
 """
 
@@ -34,7 +34,7 @@ class SpeechSegment(AudioSegment):
 
     def partition(self, max_size, format=None):
         """
-        Partitions audio segments in moments of silence according to a 
+        Partitions audio segments in moments of silence according to a
         specific heuristic.
         max_size: int (in bytes)
         """
@@ -45,40 +45,44 @@ class SpeechSegment(AudioSegment):
             size = len(self.export(format=format).read())
             n_partitions = ceil(size / max_size)
 
-        max_length = ceil(self.frame_count() / n_partitions)
-        conv_width = 1 + self.frame_rate // 2
-        conv_offset = self.frame_rate // 8
-        slack = max_length // 4
+        if n_partitions > 1:
+            max_length = ceil(self.frame_count() / n_partitions)
+            conv_width = 1 + self.frame_rate // 2
+            conv_offset = self.frame_rate // 8
+            slack = max_length // 8
 
-        # compute the amplitudes and the convolution
-        waveform = self._waveform.mean(axis=1)
-        amplitudes = waveform ** 2
-        convolved_amplitudes, mapping = convolve(
-            amplitudes,
-            conv_width,
-            conv_offset
-        )
-
-        # find the mid-points
-        internal_length = max_length - 2 * slack
-        n_partitions = ceil(self.frame_count() / internal_length)
-        mid_points = [i * internal_length for i in range(1, n_partitions - 1)]
-
-        # find a most silent point around the mid-points, this is where the 
-        # waveform will be sliced
-        cut_points = [None] * (n_partitions + 1)
-        cut_points[0] = 0
-        cut_points[-1] = self.frame_count() - 1
-        for i, mid_point in enumerate(mid_points):
-            cut_points[i + 1] = get_silence(
-                convolved_amplitudes,
-                mapping,
-                mid_point - slack,
-                mid_point + slack
+            # compute the amplitudes and the convolution
+            waveform = self._waveform.mean(axis=1)
+            amplitudes = waveform ** 2
+            convolved_amplitudes, mapping = convolve(
+                amplitudes,
+                conv_width,
+                conv_offset
             )
 
-        # partition the waveform
-        for i in range(len(cut_points) - 1):
-            waveform = self._waveform[cut_points[i]: cut_points[i + 1]]
-            segment = self._spawn(data = waveform)
-            yield segment
+            # find the mid-points
+            internal_length = max_length - 2 * slack
+            n_partitions = ceil(self.frame_count() / internal_length)
+            mid_points = [i * internal_length for i in range(1, n_partitions)]
+
+            # find a most silent point around the mid-points, this is where the
+            # waveform will be sliced
+            cut_points = [None] * (n_partitions + 1)
+            cut_points[0] = 0
+            cut_points[-1] = int(self.frame_count() - 1)
+            for i, mid_point in enumerate(mid_points):
+                cut_points[i + 1] = int(
+                    get_silence(
+                        convolved_amplitudes,
+                        mapping,
+                        mid_point - slack,
+                        mid_point + slack
+                    )
+                )
+            # partition the waveform
+            for i in range(len(cut_points) - 1):
+                waveform = self._waveform[cut_points[i]: cut_points[i + 1]]
+                segment = self._spawn(data = waveform)
+                yield segment
+        else:
+            yield self
