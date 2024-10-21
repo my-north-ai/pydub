c698a205a7876e48eab267469ad24f1438e48d01
algmarques <104544064+algmarques@users.noreply.github.com>
2024-10-21 11:06:30 +0100

    update utils.py

--- pydub/utils.py
+++ pydub/utils.py
@@ -54,7 +54,7 @@ def get_min_max_value(bit_depth):
     return ARRAY_RANGES[bit_depth]
 
 
-def _fd_or_path_or_tempfile(fd, mode='rw+b', tempfile=True):
+def _fd_or_path_or_tempfile(fd, mode='w+b', tempfile=True):
     close_fd = False
     if fd is None and tempfile:
         fd = TemporaryFile(mode=mode)
